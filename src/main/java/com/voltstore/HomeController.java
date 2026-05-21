8af40a4ab8c2e13389a0a8a4f137f9d260293bb2
abhskfnd@gmail.com <abhirambirakati.com>
2026-05-22 00:10:04 +0530

    new changes

--- src/main/java/com/voltstore/HomeController.java
+++ src/main/java/com/voltstore/HomeController.java
@@ -1,13 +1,18 @@
 package com.voltstore;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.security.core.Authentication;
 
 @Controller
 public class HomeController {
 
     @GetMapping("/")
-    public String home() {
+    public String home(Authentication auth, Model model) {
+        if (auth != null && auth.getName() != null) {
+            model.addAttribute("userName", auth.getName());
+        }
         return "index";
     }
 }
